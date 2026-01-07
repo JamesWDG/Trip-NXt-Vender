88bd13b805bba451f29584c4fff18d515bc9e115
James Howard <james@webdesignglory.com>
2026-01-08 03:48:03 +0500

    google map configured on ios

--- ios/TripNxtVendor/AppDelegate.swift
+++ ios/TripNxtVendor/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyD28UEoebX1hKscL3odt2TiTRVfe5SSpwE")
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
--- src/contants/api.ts
+++ src/contants/api.ts
@@ -8,20 +8,23 @@ type endpointTypes = {
     RESET_PASSWORD: string;
     GET_USER_PROFILE: string;
     UPDATE_USER_PROFILE: (id: number) => string;
+    CREATE_RESTAURANT: string;
 }
 
-export const BASE_URL: string = 'https://api.trip-nxt.com/api/v1/user/' //live
+export const BASE_URL: string = 'https://api.trip-nxt.com/api/v1' //live
 // export const BASE_URL: string = 'https://immaterial-overfrequently-audrie.ngrok-free.dev/api/v1/user/' //local 
 
 export const endpoint: endpointTypes = Object.freeze({
-    LOGIN: 'login',
-    SIGNUP: 'register',
-    LOGOUT: 'logout',
-    FORGOT_PASSWORD: 'forget-password',
-    VERIFY_OTP: 'verify-otp',
-    RESEND_OTP: 'resend-otp',
-    RESET_PASSWORD: 'reset-password',
-    GET_USER_PROFILE: 'get-auth-user',
-    UPDATE_USER_PROFILE: (id) => `update-user/${id}`,
+    LOGIN: '/user/login',
+    SIGNUP: '/user/register',
+    LOGOUT: '/user/logout',
+    FORGOT_PASSWORD: '/user/forget-password',
+    VERIFY_OTP: '/user/verify-otp',
+    RESEND_OTP: '/user/resend-otp',
+    RESET_PASSWORD: '/user/reset-password',
+    GET_USER_PROFILE: '/user/get-auth-user',
+    UPDATE_USER_PROFILE: (id) => `/user/update-user/${id}`,
+    CREATE_RESTAURANT: '/restaurant/create',
+
     // UPDATE_USER_PROFILE: (data: any) => `update-user/${data?.id}?profilePicture=${data?.profilePicture}&name=${data?.name}&phoneNumber=${data?.phoneNumber}`,
 })
